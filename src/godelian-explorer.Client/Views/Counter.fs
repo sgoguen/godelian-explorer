5f3a7e3c201740dc6814b7899fa3a2e0354df86d
Steve Goguen <sgoguen@gmail.com>
2025-03-01 17:14:47 -0500

    Updating the counter example to use the Godelian Toolkit

--- src/godelian-explorer.Client/Views/Counter.fs
+++ src/godelian-explorer.Client/Views/Counter.fs
@@ -8,26 +8,72 @@ module Counter =
     open Model
     open Model.Messages
 
+    open GodelianToolkit
+    open GodelianToolkit.AutoConstructor
+
+    type Term =
+        | Value of bigint
+        | Add of Term * Term
+        | Mul of Term * Term
+
+    let rec toNode (term: Term) : Bolero.Node =
+        match term with
+        | Value b -> text (b.ToString())
+        | Add(l, r) ->
+            concat {
+                toNode l
+                text " + "
+                toNode r
+            }
+        | Mul(l, r) ->
+            concat {
+                toNode l
+                text " * "
+                toNode r
+            }
+
+    //  Now, let's see what we can do to implement it for this...
+    let pick = createGodelianConstructorFor<Term> ()
 
     let counterPage model dispatch =
         concat {
-            h1 { attr.``class`` "title"; "A simple counter" }
+            h1 {
+                attr.``class`` "title"
+                "A simple counter!!!!"
+            }
+
             p {
                 button {
                     on.click (fun _ -> dispatch Decrement)
                     attr.``class`` "button"
                     "-"
                 }
+
                 input {
                     attr.``type`` "number"
                     attr.id "counter"
                     attr.``class`` "input"
                     bind.input.int model.counter (fun v -> dispatch (SetCounter v))
                 }
+
                 button {
                     on.click (fun _ -> dispatch Increment)
                     attr.``class`` "button"
                     "+"
+                }
+            }
+
+            div {
+                let term = pick model.counter
+
+                h2 {
+                    attr.``class`` "subtitle"
+                    "Term"
+                }
+
+                div {
+                    attr.``class`` "box"
+                    toNode term
                 }
             }
         }
